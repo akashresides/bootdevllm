38c06598bdb4856f9502618662a2a90247664377
akash <akashresides@gmail.com>
2025-07-07 20:13:11 +1000

    verbose flag added

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+def main():
+	if len(sys.argv) < 2:
+		print("Error: no prompt string provided as argument")
+		sys.exit(1)
+	user_prompt = sys.argv[1]
+	messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+	response = client.models.generate_content(
+	model='gemini-2.0-flash-001', contents=messages,
+	)
+	print(response.text)
+	vflag = False
+	for arg in sys.argv:
+		if arg == '--verbose':
+			vflag = True
+	if vflag:
+		print(f"User prompt: {user_prompt}")
+		print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+		print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
